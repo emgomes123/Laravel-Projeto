d907dfda60216f4afe1ab27bf4b0fcc1b553586f
Edson <edsomar.pr739@hotmail.com>
2020-04-27 16:03:38 -0300

    teste

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -13,7 +13,7 @@ class User extends Model
     use SoftDeletes;
     
     public $timestamps = true;
-    protected $table = 'user';
+    protected $table = 'users';
 
     protected $fillable = ['cpf', 'nome', 'phone','birth','gender','notes','email','password','status','permission'];
     protected $hidden = ['password', 'remember_token'];
